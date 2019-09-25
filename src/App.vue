3d48b43dd37302aac6634f3e66af557ef81d699d
Matthieu <matthieu.zandecki@gmail.com>
2019-09-25 22:37:04 +0100

    add router

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,12 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <ApolloExample msg="Welcome to Your Vue.js App"/>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
-
-<script>
-import ApolloExample from './components/ApolloExample.vue'
-
-export default {
-  name: 'app',
-  components: {
-    ApolloExample
-  }
-}
-</script>
 
 <style>
 #app {
