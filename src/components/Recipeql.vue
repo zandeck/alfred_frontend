bb2385ed45fbf26a54f74a5e3fb26652facb6380
Matthieu <matthieu.zandecki@gmail.com>
2019-10-02 09:46:14 +0100

    clean from example files

--- src/components/Recipeql.vue
+++ src/components/Recipeql.vue
@@ -4,15 +4,12 @@
 
 <script>
 import gql from "graphql-tag";
+import API_VERSION from "../graphql/Version.gql";
 
 export default {
   name: "Recipeql",
   apollo: {
-    apiVersion: gql`
-      query {
-        apiVersion
-      }
-    `
+    apiVersion: API_VERSION
   }
 };
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,11 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
     <Recipeql></Recipeql>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 import Recipeql from "@/components/Recipeql.vue";
 
 export default {
